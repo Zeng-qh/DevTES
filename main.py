81492b9a1efb6512a4673c251579965716133061
都一样 <976272457@qq.com>
2022-08-11 13:53:09 +0800

    ✨ feat: 添加参数以及添加模板

--- main.py
+++ main.py
@@ -7,25 +7,43 @@ import os
 import random
 
 today = datetime.now()
-start_date = os.environ['START_DATE']
-city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+start_date = os.environ['START_DATE'] # 日期格式的字符串 "2018-01-11"
+city = os.environ['CITY'] # 地区字符串 "深圳"
+birthday = os.environ['BIRTHDAY'] # 日期字符串  "08-16"
+Legal_start=os.environ['LEGAL_START'] # 日期格式的字符串 "2018-01-11"
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
-
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
+
+# 对应模板格式
+# {{today.DATA}}  {{week.DATA}}  
+# {{festival.DATA}}  
+# 天气: {{weather.DATA}} 
+# 最低温度:{{low.DATA}}
+# 最高温度:{{high.DATA}}
+# 今天是我们恋爱的第:{{love_days.DATA}} 天
+# 我们已经成为合法夫妇:{{Legal_couple.DATA }} 天
+# 距离你的生日还有:{{birthday_left.DATA}}天
+
+# {{words.DATA}}
+
+# {{Text.DATA}}
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  return weather['weather'], math.floor(weather['low']),math.floor(weather['high'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  return delta.days
+
+def get_Legal_couple_days():
+  delta = today - datetime.strptime(Legal_start, "%Y-%m-%d")
   return delta.days
 
 def get_birthday():
@@ -47,7 +65,25 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+wea,low,high  = get_weather()
+weekList=["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+todaystr=today.strftime("%Y-%m-%d") 
+weekSrt=weekList[datetime.now().weekday()]
+festival="周末愉快!" if datetime.now().weekday()>=6 else ""
+
+data = {
+  "today":{"value":todaystr},
+  "week":{"value":weekSrt},
+  "festival":{"value":festival},
+  "weather":{"value":wea},
+  "low":{"value":low},
+  "high":{"value":high}, 
+  "love_days":{"value":get_count()},
+  "Legal_couple":{"value": get_Legal_couple_days()},# 空字符或数值
+  "birthday_left":{"value":get_birthday()},
+  "words":{"value":get_words(), "color":get_random_color()},
+  "Text":{"value":get_words(), "color":get_random_color()}
+}
 res = wm.send_template(user_id, template_id, data)
+print(data)
 print(res)
