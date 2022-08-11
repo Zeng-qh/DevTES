deef32f02f93eb7e6beda2f74f7426ce164151af
都一样 <976272457@qq.com>
2022-08-11 14:56:07 +0800

    发送消息

--- main.py
+++ main.py
@@ -86,12 +86,13 @@ data = {
   "Legal_couple":{"value": get_Legal_couple_days()},# 空字符或数值
   "birthday_left":{"value":get_birthday()},
   "words":{"value":get_words(), "color":get_random_color()},
-  "Text":{"value":get_words(), "color":get_random_color()},
-  "Legal_start":{"key":Legal_start}
+  "Text":{"value":get_words(), "color":get_random_color()}
 }
 
-# client = WeChatClient(app_id, app_secret)
-#wm = WeChatMessage(client)
-# res = wm.send_template(user_id, template_id, data)
+
 print(data)
-# print(res)
+
+client = WeChatClient(app_id, app_secret)
+wm = WeChatMessage(client)
+res = wm.send_template(user_id, template_id, data)
+print(res)
